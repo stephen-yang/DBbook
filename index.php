e8dc6c8ddc2c187f64b2b983fd74b6dded10a1c3
stephen.yang <stephen.yang@dadaabc.com>
2017-12-15 16:40:31 +0800

    camel case

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ $port = $config['port'];
 $username = $config['username'];
 $password = $config['password'];
 $database = $config['database'];
-$dbdoc_path_perfix = './dbbook/';
-$dbdoc_path = $dbdoc_path_perfix . $database . '/';
+$docPathPrefix = './dbbook/';
+$docPath = $docPathPrefix . $database . '/';
 
 // 创建文件夹
 function mkdirs($dir, $mode = 0777)
@@ -22,50 +22,50 @@ function mkdirs($dir, $mode = 0777)
     return @mkdir($dir, $mode);
 }
 
-mkdirs($dbdoc_path);
+mkdirs($docPath);
 
-$db_connect = new mysqli($host, $username, $password, $database, $port);
+$connect = new mysqli($host, $username, $password, $database, $port);
 
 // 获取表基础信息
 $sql = 'SELECT TABLE_SCHEMA, TABLE_NAME, `ENGINE`, CREATE_TIME, TABLE_COLLATION,TABLE_COMMENT FROM information_schema.`TABLES` WHERE TABLE_SCHEMA = "' . $database . '"';
 
-$result = $db_connect->query($sql);
+$result = $connect->query($sql);
 while ($row = mysqli_fetch_assoc($result)) {
-    $table_base_data[$row['TABLE_NAME']] = $row;
+    $tableBaseData[$row['TABLE_NAME']] = $row;
 }
 
-//file_put_contents('./table_base_data.json',json_encode($table_base_data));
+//file_put_contents('./table_base_data.json',json_encode($tableBaseData));
 
-$len = count($table_base_data);
+$len = count($tableBaseData);
 
 // 获取表字段信息
-foreach ($table_base_data as $key => $val) {
+foreach ($tableBaseData as $key => $val) {
     static $i = 1;
     echo '获取表信息' . $i . '/' . $len . "\n";
     $sql = 'SELECT COLUMN_NAME,COLUMN_TYPE,COLUMN_KEY,COLUMN_DEFAULT,IS_NULLABLE,COLUMN_COMMENT FROM INFORMATION_SCHEMA. COLUMNS WHERE `TABLE_SCHEMA` = "' . $database . '" AND `TABLE_NAME` = "' . $val['TABLE_NAME'] . '"';
-    $result = $db_connect->query($sql);
+    $result = $connect->query($sql);
     while ($row = mysqli_fetch_assoc($result)) {
-        $table_column_data[$val['TABLE_NAME']][] = $row;
+        $tableColumnData[$val['TABLE_NAME']][] = $row;
     }
     $i++;
 }
 
 // 处理特殊字符
-$table_column_data_json = json_encode($table_column_data);
+$tableColumnDataJson = json_encode($tableColumnData);
 
-$table_column_data_json = str_replace('|', '｜', $table_column_data_json);
-$table_column_data_json = str_replace('---', '－－－', $table_column_data_json);
-$table_column_data_json = str_replace('\r\n', '<br>', $table_column_data_json);
-$table_column_data_json = str_replace('\n', '<br>', $table_column_data_json);
+$tableColumnDataJson = str_replace('|', '｜', $tableColumnDataJson);
+$tableColumnDataJson = str_replace('---', '－－－', $tableColumnDataJson);
+$tableColumnDataJson = str_replace('\r\n', '<br>', $tableColumnDataJson);
+$tableColumnDataJson = str_replace('\n', '<br>', $tableColumnDataJson);
 
-$table_column_data = json_decode($table_column_data_json);
+$tableColumnData = json_decode($tableColumnDataJson);
 
 // 生成 markdown
-$table_name = '';
-$base_title = '## 基础信息' . PHP_EOL;
-$column_title = '## 字段信息' . PHP_EOL;
+$tableName = '';
+$baseTitle = '## 基础信息' . PHP_EOL;
+$columnTitle = '## 字段信息' . PHP_EOL;
 
-$base_th_array = [
+$baseThArray = [
     'TABLE_SCHEMA' => '数据库',
     'TABLE_NAME' => '表名',
     'ENGINE' => '表引擎',
@@ -74,7 +74,7 @@ $base_th_array = [
     'TABLE_COMMENT' => '表注释',
 ];
 
-$column_th_array = [
+$columnThArray = [
     'COLUMN_NAME' => '字段',
     'COLUMN_TYPE' => '类型',
     'COLUMN_KEY' => '索引',
@@ -84,11 +84,11 @@ $column_th_array = [
 ];
 
 // gitbook README.md
-file_put_contents($dbdoc_path_perfix . 'README.md', '# 数据库文档' . PHP_EOL . PHP_EOL);
-file_put_contents($dbdoc_path_perfix . 'README.md', '更新时间: ' . date('Y-m-d H:i:s', time()) . PHP_EOL . PHP_EOL, FILE_APPEND);
+file_put_contents($docPathPrefix . 'README.md', '# 数据库文档' . PHP_EOL . PHP_EOL);
+file_put_contents($docPathPrefix . 'README.md', '更新时间: ' . date('Y-m-d H:i:s', time()) . PHP_EOL . PHP_EOL, FILE_APPEND);
 
 // book.json
-$book_json = '{
+$bookJson = '{
     "plugins": [
         "-sharing",
         "-lunr",
@@ -97,55 +97,55 @@ $book_json = '{
     ]
 }';
 
-file_put_contents($dbdoc_path_perfix . 'book.json', $book_json);
+file_put_contents($docPathPrefix . 'book.json', $bookJson);
 
 // gitbook SUMMARY.md 标题
-file_put_contents($dbdoc_path_perfix . 'SUMMARY.md', '# Summary' . PHP_EOL . PHP_EOL);
+file_put_contents($docPathPrefix . 'SUMMARY.md', '# Summary' . PHP_EOL . PHP_EOL);
 
 echo "准备生成 markdown 文档...";
 
 // 表基础信息
-foreach ($table_base_data as $key => $value) {
-    $base_th_str = '';
-    $base_th_split = '';
-    $base_td_str = '';
-    $table_name = '# ' . $key . PHP_EOL;
-    foreach ($value as $c_key => $c_value) {
-        $base_th_str .= '|' . $base_th_array[$c_key];
-        $base_th_split .= '|---';
-        $base_td_str .= '|' . $c_value;
+foreach ($tableBaseData as $key => $value) {
+    $baseThStr = '';
+    $baseThSplit = '';
+    $baseTdStr = '';
+    $tableName = '# ' . $key . PHP_EOL;
+    foreach ($value as $cKey => $cValue) {
+        $baseThStr .= '|' . $baseThArray[$cKey];
+        $baseThSplit .= '|---';
+        $baseTdStr .= '|' . $cValue;
     }
 
     // gitbook SUMMARY.md 目录
     $summary = '* [' . $key . '](' . $database . '/' . $key . '.md)';
-    file_put_contents($dbdoc_path_perfix . 'SUMMARY.md', $summary . PHP_EOL, FILE_APPEND);
+    file_put_contents($docPathPrefix . 'SUMMARY.md', $summary . PHP_EOL, FILE_APPEND);
 
-    file_put_contents($dbdoc_path . $key . '.md', $table_name . PHP_EOL);
-    file_put_contents($dbdoc_path . $key . '.md', $base_title . PHP_EOL, FILE_APPEND);
-    file_put_contents($dbdoc_path . $key . '.md', $base_th_str . '|' . PHP_EOL, FILE_APPEND);
-    file_put_contents($dbdoc_path . $key . '.md', $base_th_split . '|' . PHP_EOL, FILE_APPEND);
-    file_put_contents($dbdoc_path . $key . '.md', $base_td_str . '|' . PHP_EOL . PHP_EOL, FILE_APPEND);
+    file_put_contents($docPath . $key . '.md', $tableName . PHP_EOL);
+    file_put_contents($docPath . $key . '.md', $baseTitle . PHP_EOL, FILE_APPEND);
+    file_put_contents($docPath . $key . '.md', $baseThStr . '|' . PHP_EOL, FILE_APPEND);
+    file_put_contents($docPath . $key . '.md', $baseThSplit . '|' . PHP_EOL, FILE_APPEND);
+    file_put_contents($docPath . $key . '.md', $baseTdStr . '|' . PHP_EOL . PHP_EOL, FILE_APPEND);
 }
 
 // 表字段信息
-foreach ($table_column_data as $key => $value) {
+foreach ($tableColumnData as $key => $value) {
     static $j = 1;
     echo '生成 markdown 文档 ' . $j . '/' . $len . "\n";
-    $column_td_str = '';
-    foreach ($value as $i_key => $i_value) {
-        $column_th_str = '';
-        $column_th_split = '';
-        foreach ($i_value as $j_key => $j_value) {
-            $column_th_str .= '|' . $column_th_array[$j_key];
-            $column_th_split .= '|---';
-            $column_td_str .= '|' . $j_value;
+    $columnTdStr = '';
+    foreach ($value as $iKey => $iValue) {
+        $columnThStr = '';
+        $columnThSplit = '';
+        foreach ($iValue as $jKey => $jValue) {
+            $columnThStr .= '|' . $columnThArray[$jKey];
+            $columnThSplit .= '|---';
+            $columnTdStr .= '|' . $jValue;
         }
-        $column_td_str .= '|' . PHP_EOL;
+        $columnTdStr .= '|' . PHP_EOL;
     }
-    file_put_contents($dbdoc_path . $key . '.md', $column_title . PHP_EOL, FILE_APPEND);
-    file_put_contents($dbdoc_path . $key . '.md', $column_th_str . '|' . PHP_EOL, FILE_APPEND);
-    file_put_contents($dbdoc_path . $key . '.md', $column_th_split . '|' . PHP_EOL, FILE_APPEND);
-    file_put_contents($dbdoc_path . $key . '.md', $column_td_str . PHP_EOL, FILE_APPEND);
-    file_put_contents($dbdoc_path . $key . '.md', '***' . PHP_EOL . PHP_EOL, FILE_APPEND);
+    file_put_contents($docPath . $key . '.md', $columnTitle . PHP_EOL, FILE_APPEND);
+    file_put_contents($docPath . $key . '.md', $columnThStr . '|' . PHP_EOL, FILE_APPEND);
+    file_put_contents($docPath . $key . '.md', $columnThSplit . '|' . PHP_EOL, FILE_APPEND);
+    file_put_contents($docPath . $key . '.md', $columnTdStr . PHP_EOL, FILE_APPEND);
+    file_put_contents($docPath . $key . '.md', '***' . PHP_EOL . PHP_EOL, FILE_APPEND);
     $j++;
 }
